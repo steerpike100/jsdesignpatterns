58781db3c2a7a37760c26cec1efded8d9de81304
stephenb <stephen.bartholomew@paxport.net>
2019-11-17 14:05:22 +0000

    Constructor PAttern

--- Constructor/task.js
+++ Constructor/task.js
@@ -3,7 +3,7 @@ this.name = name;
 this.completed = false;
 
 this.complete = function(){
-    console.log('completing task' + task.name);
+    console.log('completing task' + this.name);
     this.completed = true;
 }
 this.save = function(){
@@ -11,8 +11,13 @@ this.save = function(){
 }
 }
 
-var task1  = new TextTrackList('create a demo for constructors')
-var task2  = new TextTrackList('create a demo for modules')
-var task3  = new TextTrackList('create a demo for singletons')
-var task4 = new TextTrackList('create a demo for prototypes')
+var task1  = new Task('create a demo for constructors');
+var task2  = new Task('create a demo for modules');
+var task3  = new Task('create a demo for singletons');
+var task4 = new Task('create a demo for prototypes');
+
+task1.complete();
+task2.save();
+task3.save();
+task4.save();
 
